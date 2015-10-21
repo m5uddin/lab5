53972eb3bfaaf7faba70099a288c2a2ad834c87a
m5uddin <masi.uddin@ryerson.ca>
2015-10-21 18:01:06 -0400

    gujhkb

---
+++ lab 5/src/lab 5.cpp
@@ -0,0 +1,17 @@
+//============================================================================
+// Name        : lab.cpp
+// Author      :
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "!!!Hello Worl\nd!!!" << endl; // prints !!!Hello World!!!
+
+	cout << "!!!Hello Worl\nd!!!" << endl; //
+	return 0;
+}
